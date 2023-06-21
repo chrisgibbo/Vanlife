64a4639a62fdfe7c85713c100b1a11f627b1ce8b
Chris Gibbons <christ487@icloud.com>
2023-06-21 21:35:02 +0100

    added about and home syntax

--- vanlife/src/App.jsx
+++ vanlife/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Home from "./pages/Home"
+import About from "./pages/About"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
+    <header>
+      <Link className="site-logo" to="/">#VanLife</Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
