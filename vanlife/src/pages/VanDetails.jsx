357d50073df1d0c9d80cd7454dbebbf58153e1dc
Chris Gibbons <christ487@icloud.com>
2023-06-24 19:32:44 +0100

    added Scrimba  quiz to notes

--- vanlife/src/pages/VanDetails.jsx
+++ vanlife/src/pages/VanDetails.jsx
@@ -29,3 +29,16 @@ function VanDetail() {
 }
 
 export default VanDetail
+
+// 1. What is a route/url parameter? - its a route that allows you to move to certain areas of the website by
+// wrapping a path with "Route, between Routes" and sets a placeholder for the URL of a page.
+// 2. Add a route parameter called "productId" to the route path below
+/* <Route path="/products/:productId" element={<ProductDetail />} /> */
+// 3. Add whatever you need to add for the component below to display
+// the route parameter in the <h1>
+// import {useParams} from "react-router-dom"
+// function ProductDetail(){ 
+//  const {productId} = useParams()
+// return <h1>Product id is {productId}<h1>
+//  return <h1>Product detail page goes here</h1>
+// }
