d88f2de064252eff17f49a16e9efeae4dfb691d1
Chris Gibbons <christ487@icloud.com>
2023-06-24 15:06:09 +0100

    Installed MirageJS

--- vanlife/src/pages/Vans.jsx
+++ vanlife/src/pages/Vans.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 function Vans() {
+  const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
+    console.log(vans)
+
+
   return (
     <div>
         <h1>Vans page goes here 🚐</h1>
