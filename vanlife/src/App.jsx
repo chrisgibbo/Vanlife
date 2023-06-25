c665b70d887441f73cc73d7de7668335b84d5858
Chris Gibbons <christ487@icloud.com>
2023-06-25 17:25:48 +0100

    rename Host component to Dashboard and relink

--- vanlife/src/App.jsx
+++ vanlife/src/App.jsx
@@ -5,7 +5,7 @@ import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetails"
 import Layout from './components/Layout'
-import Host from './pages/Host/Host'
+import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
 
@@ -20,7 +20,7 @@ function App() {
         <Route path="/about" element={<About/>} />
         <Route path="/vans" element={<Vans/>} />
         <Route path="/vans/:id" element={<VanDetail />} />
-        <Route path="/host" element={<Host />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/income" element={<Income />} />
         <Route path="/reviews" element={<Reviews />} />
       </Route>
--- vanlife/src/pages/Host/Host.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Host() {
-  return (
-    <h1>Host Dashboard Here</h1>
-    )
-}
-
-export default Host
