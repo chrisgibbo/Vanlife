77029b6b331ad3ed727671295e588c445995dd2f
Chris Gibbons <christ487@icloud.com>
2023-06-25 16:37:59 +0100

    added Outlet to syntax

--- vanlife/src/components/Layout.jsx
+++ vanlife/src/components/Layout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
+
 function Layout() {
   return (
-    <h1>This is the layout Route</h1>
+    <>
+        <h1>This is the layout Route</h1>
+        <Outlet />
+    </>
     )
 }
 
