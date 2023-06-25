4444a9db5479d525dc22282449d9e2979548eb60
Chris Gibbons <christ487@icloud.com>
2023-06-25 16:30:58 +0100

    Import Layout

--- vanlife/src/App.jsx
+++ vanlife/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetails"
+import Layout from './components/Layout'
 
 import "../server"
 
@@ -11,10 +12,12 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/vans" element={<Vans/>} />
-      <Route path="/vans/:id" element={<VanDetail />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/vans" element={<Vans/>} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+      </Route>
     </Routes>
   </BrowserRouter>
   )
