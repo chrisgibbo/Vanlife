78c1b50570bf6a636ae3d988e9079f688c9a46db
Chris Gibbons <christ487@icloud.com>
2023-06-25 17:04:11 +0100

    Import host, income and reviews to App.jsx

--- vanlife/src/App.jsx
+++ vanlife/src/App.jsx
@@ -5,6 +5,9 @@ import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetails"
 import Layout from './components/Layout'
+import Host from './pages/Host/Host'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
 
 import "../server"
 
@@ -17,6 +20,9 @@ function App() {
         <Route path="/about" element={<About/>} />
         <Route path="/vans" element={<Vans/>} />
         <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/host" element={<Host />} />
+        <Route path="/income" element={<Income />} />
+        <Route path="/reviews" element={<Reviews />} />
       </Route>
     </Routes>
   </BrowserRouter>
