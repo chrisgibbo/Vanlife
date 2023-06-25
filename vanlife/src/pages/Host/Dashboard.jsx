25d4cf5f154bb2120d5f738d804d16dbddd77a70
Chris Gibbons <christ487@icloud.com>
2023-06-25 19:39:21 +0100

    added Outlet to Dashboard page

--- vanlife/src/pages/Host/Dashboard.jsx
+++ vanlife/src/pages/Host/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function Dashboard() {
   return (
-    <h1>Dashboard goes Here</h1>
+    <>
+      <h1>Dashboard goes Here</h1>
+      <Outlet />
+    </>
     )
 }
 
