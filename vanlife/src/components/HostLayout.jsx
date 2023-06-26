7820bec0878349cf11ad9bb6f9b4bd0de9523271
Chris Gibbons <christ487@icloud.com>
2023-06-26 21:21:56 +0100

    Add the nav component in HostLayout with Link

--- vanlife/src/components/HostLayout.jsx
+++ vanlife/src/components/HostLayout.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 function HostLayout() {
   return (
-    <div>HostLayout</div>
-  )
+    <>
+        <nav>
+            <Link to="/host">Dashboard</Link>
+            <Link to="/host/income">Income</Link>
+            <Link to="/host/reviews">Reviews</Link>
+        </nav>
+    </>
+    )
 }
 
 export default HostLayout
