723395d955b1c986ffaca958e02c11b543dd2655
Chris Gibbons <christ487@icloud.com>
2023-06-27 21:11:32 +0100

    remove Link, as NavlINK REPLACES

--- vanlife/src/components/HostLayout.jsx
+++ vanlife/src/components/HostLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Outlet, NavLink } from 'react-router-dom'
+import { Outlet, NavLink } from 'react-router-dom'
 
 function HostLayout() {
     const currentActiveStyle = {
