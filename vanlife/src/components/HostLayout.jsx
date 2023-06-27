d4c8477d941c2e914e748c48211ed6e69e56d6eb
Chris Gibbons <christ487@icloud.com>
2023-06-27 21:41:33 +0100

    seperated NavLink fo readability

--- vanlife/src/components/HostLayout.jsx
+++ vanlife/src/components/HostLayout.jsx
@@ -14,12 +14,15 @@ function HostLayout() {
             <NavLink to="/host" end
             style={({isActive}) => isActive ? activeStyles :null}
             >Dashboard</NavLink>
+
             <NavLink to="/host/income"
             style={({isActive}) => isActive ? activeStyles :null}
             >Income</NavLink>
+
             <NavLink to="/host/vans"
             style={({isActive}) => isActive ? activeStyles :null}
             >Vans</NavLink>
+            
             <NavLink to="/host/reviews"
             style={({isActive}) => isActive ? activeStyles :null}
             >Reviews</NavLink>
