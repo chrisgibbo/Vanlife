114a13ebd9e82f9129b03703f975731a033db1d8
Chris Gibbons <christ487@icloud.com>
2023-06-27 20:47:05 +0100

    removed className, kept style

--- vanlife/src/components/Header.jsx
+++ vanlife/src/components/Header.jsx
@@ -15,15 +15,12 @@ function Header() {
     <Link className="site-logo" to="/">#VanLife</Link>
     <nav>
       <NavLink to="/host" 
-        className={({isActive}) => isActive ? " " : null}
         style={({isActive}) => isActive ? activeStyle : null}
       >Host</NavLink>
       <NavLink to="/about"
-        className={({isActive}) => isActive ? " " : null}
         style={({isActive}) => isActive ? activeStyle : null}
       >About</NavLink>
       <NavLink to="/vans"
-        className={({isActive}) => isActive ? " " : null}
         style={({isActive}) => isActive ? activeStyle : null}
       >Vans</NavLink>
     </nav>
