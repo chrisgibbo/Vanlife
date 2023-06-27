7452b482e232037b1e156ba4a9c4d437108dd213
Chris Gibbons <christ487@icloud.com>
2023-06-27 21:32:30 +0100

    added Vans to nav links

--- vanlife/src/components/HostLayout.jsx
+++ vanlife/src/components/HostLayout.jsx
@@ -17,6 +17,9 @@ function HostLayout() {
             <NavLink to="/host/income"
             style={({isActive}) => isActive ? activeStyles :null}
             >Income</NavLink>
+            <NavLink to="/host/vans"
+            style={({isActive}) => isActive ? activeStyles :null}
+            >Vans</NavLink>
             <NavLink to="/host/reviews"
             style={({isActive}) => isActive ? activeStyles :null}
             >Reviews</NavLink>
