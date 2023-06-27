714e51cb68bf963a6951674ec5b92a92b51e9deb
Chris Gibbons <christ487@icloud.com>
2023-06-27 21:26:35 +0100

    added HostVCans import, + Rote

--- vanlife/src/App.jsx
+++ vanlife/src/App.jsx
@@ -9,6 +9,8 @@ import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
 import HostLayout from './components/HostLayout'
+import HostVans from './pages/Host/HostVans'
+
 
 import "../server"
 
@@ -26,6 +28,7 @@ function App() {
           <Route index element={<Dashboard />} />
           <Route path="income" element={<Income />} />
           <Route path="reviews" element={<Reviews />} />
+          <Route path="vans" element={<HostVans />} />
         </Route>
       </Route>
     </Routes>
