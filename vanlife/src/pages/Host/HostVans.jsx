7994ce733d2303f366870052146434fe23392e54
Chris Gibbons <christ487@icloud.com>
2023-06-28 19:38:16 +0100

    add const + react.useEffect/fetch request

--- vanlife/src/pages/Host/HostVans.jsx
+++ vanlife/src/pages/Host/HostVans.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function HostVans() {
+  const [vans, setVans] = React.useState([])
 
+  React.useEffect(() => {
+    fetch("api/host/vans")
+    .then(res => res.json())
+    .then(data => setVans(data.vans))
+  }, [])
 
 
 
