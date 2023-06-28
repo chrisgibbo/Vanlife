56dda5f237ac91eece2e06d55bb765ffffc2bab9
Chris Gibbons <christ487@icloud.com>
2023-06-28 20:50:18 +0100

    create function with return features from API

--- vanlife/src/pages/Host/HostVanDetail.jsx
+++ vanlife/src/pages/Host/HostVanDetail.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function HostVanDetail() {
+  const { id } = useParams()
+  const [currentVan, setCurrentVan] = React.useState(null)
+
+  React.useEffect(() => {
+      fetch(`/api/host/vans/${id}`)
+        .then(res => res.json())
+        .then(data => setCurrentVan(data.vans))
+  }, [])
+
+  if (!currentVan) {
+    return <h1>Loading ...</h1>
+  }
+ 
   return (
-    <h1>Host van detail page</h1>
+    <div>
+      <img src={currentVan.imageUrl} width={150} />
+      <h2>{currentVan.name}</h2>
+      <p>{currentVan.price}</p>
+      <p>{currentVan.type}</p>
+
+    </div>
   )
 }
 
