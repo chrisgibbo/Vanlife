9cac7600cfcab6162a6d9b0c4c218e7fb32e7af6
Chris Gibbons <christ487@icloud.com>
2023-06-28 20:11:39 +0100

    missed "/" on fetch before api...

--- vanlife/src/pages/Host/HostVans.jsx
+++ vanlife/src/pages/Host/HostVans.jsx
@@ -5,46 +5,49 @@ function HostVans() {
   const [vans, setVans] = React.useState([])
 
   React.useEffect(() => {
-    fetch("api/host/vans")
-    .then(res => res.json())
-    .then(data => setVans(data.vans))
-  }, [])
+      fetch("/api/host/vans")
+        .then(res => res.json())
+        .then(data => setVans(data.vans))
+    }, [])
 
   const hostVansEls = vans.map(van => (
-    <Link
-      to={`/host/vans${vans.id}`}
-      key={van.id}
-      className="host-van-link-wrapper"
+      <Link
+          to={`/host/vans${vans.id}`}
+          key={van.id}
+          className="host-van-link-wrapper"
       >
 
-      <div className="host-van-single" key={van.id}>
-        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-        <div className="host-van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}/day</p>
-        </div>
-      </div>    
-    </Link>
+          <div className="host-van-single" key={van.id}>
+              <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+          </div>    
+      </Link>
   ))
   
   return (
-    <section>
-      <h1 className='host-vans-title'>Yourlisted vans</h1>
-      <div className='host-vans-list'>
-        {
-          vans.length > 0 ? (
-            <section>
-              {hostVansEls}
-            </section>
-          ) : (
-            <h2>Loading ...</h2>
-          )     
-   
-        }
-      </div>
-    </section>
+      <section>
+        <h1 className="host-vans-title">Yourlisted vans</h1>
+          <div className="host-vans-list">
+              {
+                vans.length > 0 ? (
+                  <section>
+                    {hostVansEls}
+                  </section>
 
-    )
-}
+                ) : (
+                      
+                      <h2>Loading ...</h2>
+                    )     
+              }
+          </div>
+      </section>
 
+      )
+  }
+
+
+  
 export default HostVans
