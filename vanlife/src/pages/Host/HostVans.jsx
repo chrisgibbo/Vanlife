64cc19e30816731c60821e3ddb6ac2bfffd8bd7c
Chris Gibbons <christ487@icloud.com>
2023-06-28 19:49:25 +0100

    added hostVCansEls const, vans.map

--- vanlife/src/pages/Host/HostVans.jsx
+++ vanlife/src/pages/Host/HostVans.jsx
@@ -10,6 +10,23 @@ function HostVans() {
     .then(data => setVans(data.vans))
   }, [])
 
+  const hostVansEls = vans.map(van => (
+    <Link
+      to={"/host/vans${vans.id}"}
+      key={van.id}
+      className="host-van-link-wrapper"
+      >
+
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}/day</p>
+        </div>
+      </div>    
+    </Link>
+  ))
+
 
 
 
