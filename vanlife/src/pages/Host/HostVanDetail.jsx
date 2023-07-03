b899f996aa3e871b4877228003e0ce90d57a389f
Chris Gibbons <christ487@icloud.com>
2023-07-03 21:35:47 +0100

    Corrected classname for css

--- vanlife/src/pages/Host/HostVanDetail.jsx
+++ vanlife/src/pages/Host/HostVanDetail.jsx
@@ -24,58 +24,58 @@ function HostVanDetail() {
  
   return (
     <section>
-    <Link
-        to=".."
-        relative='path'
-        className="back-button"
-      >&larr; <span>Back to all vans</span></Link>
+        <Link
+            to=".."
+            relative='path'
+            className="back-button"
+          >&larr; <span>Back to all vans</span></Link>
 
 
-    <div className="host-van-detail-layout-container">
-        <div className="host-van-detail">
-            <img src={currentVan.imageUrl} />
-            <div className="host-van-detail-info-text">
-                <i
-                    className={`van-type van-type-${currentVan.type}`}
-                >
-                    {currentVan.type}
-                </i>
-                <h3>{currentVan.name}</h3>
-                <h4>${currentVan.price}/day</h4>
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+
+                <nav className="host-van-detail-nav">
+                  <NavLink
+                    to="."
+                    end
+                    style={({isActive}) => isActive ? activeStyles : null}
+                  >
+                    Details
+                  </NavLink>
+
+                  <NavLink
+                    to="pricing"
+                    end
+                    style={({isActive}) => isActive ? activeStyles : null}
+                  >
+                    Pricing
+                  </NavLink>
+
+                  <NavLink
+                    to="photos"
+                    end
+                    style={({isActive}) => isActive ? activeStyles : null}
+                  >
+                    Photos
+                  </NavLink>
+
+                </nav>
+
+
+
+                <Outlet/>
             </div>
-        </div>
-
-        <nav className="host-van-detail-van">
-          <NavLink
-            to="."
-            end
-            style={({isActive}) => isActive ? activeStyles : null}
-          >
-            Details
-          </NavLink>
-
-          <NavLink
-            to="pricing"
-            end
-            style={({isActive}) => isActive ? activeStyles : null}
-          >
-            Pricing
-          </NavLink>
-
-          <NavLink
-            to="photos"
-            end
-            style={({isActive}) => isActive ? activeStyles : null}
-          >
-            Photos
-          </NavLink>
-
-        </nav>
-
-
-
-        <Outlet/>
-    </div>
 </section>
   )
 }
