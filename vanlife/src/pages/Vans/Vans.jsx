b8a26dfe992e3bde36f3cf7b9fa62a13885eb3cf
Chris Gibbons <christ487@icloud.com>
2023-07-04 20:13:33 +0100

    add typefilter and displayedVans to map

--- vanlife/src/pages/Vans/Vans.jsx
+++ vanlife/src/pages/Vans/Vans.jsx
@@ -13,7 +13,11 @@ function Vans() {
             .then(data => setVans(data.vans))
     }, [])
 
-    const vanElements = vans.map(van => (
+    const displayedVans = typeFilter
+    ? vans.filter(van => van.type === typeFilter)
+    : vans
+
+    const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
                 <img src={van.imageUrl} />
