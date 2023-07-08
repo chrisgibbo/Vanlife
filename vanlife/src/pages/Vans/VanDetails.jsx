63383801725d13415ff620b1ff9eb8f09ce9dd52
Chris Gibbons <christ487@icloud.com>
2023-07-08 13:51:53 +0100

    amend syntax to Scrimba, fault in code

--- vanlife/src/pages/Vans/VanDetails.jsx
+++ vanlife/src/pages/Vans/VanDetails.jsx
@@ -4,6 +4,7 @@ import { useParams, Link, useLocation } from 'react-router-dom'
 function VanDetail() {
     const params = useParams()
     const location = useLocation()
+
     const [van, setVan] = React.useState(null)
 
     React.useEffect(() => {
@@ -12,28 +13,29 @@ function VanDetail() {
             .then(data => setVan(data.vans))
 }, [params.id]) 
 
-    const search = location.state?.search || ""
-
-    return (
-        <div className="van-detail-container">
-            <Link
-                    to={`..${search}`}
-                    relative="path"
-                    className="back-button"
-                >&larr; <span>Back to all vans</span>
-            </Link>
-
-            {van ? (
-                <div className="van-detail">
-                    <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                    <h2>{van.name}</h2>
-                    <p className="van-price"><span>${van.price}</span>/day</p>
-                    <p>{van.description}</p>
-                    <button className="link-button">Rent this van</button>               
-                </div>
-            ) : <h2>Loading ....</h2>}
-        </div>
+const search = location.state?.search || ""
+    
+return (
+    <div className="van-detail-container">
+        <Link
+            to={`..${search}`}
+            relative="path"
+            className="back-button"
+        >&larr; <span>Back to all vans</span></Link>
+        
+        {van ? (
+            <div className="van-detail">
+                <img src={van.imageUrl} />
+                <i className={`van-type ${van.type} selected`}>
+                    {van.type}
+                </i>
+                <h2>{van.name}</h2>
+                <p className="van-price"><span>${van.price}</span>/day</p>
+                <p>{van.description}</p>
+                <button className="link-button">Rent this van</button>
+            </div>
+        ) : <h2>Loading...</h2>}
+    </div>
         )
 }
 
