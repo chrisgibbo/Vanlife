96f66d17531ee031a63ceb90d3ab5f14ce591b32
Chris Gibbons <christ487@icloud.com>
2023-07-10 21:45:59 +0100

    import useRouteError add const

--- vanlife/src/components/Error.jsx
+++ vanlife/src/components/Error.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom'
+
 
 function Error() {
+    const error = useRouteError()
   return (
     <h1>An error occured!</h1>
   )
