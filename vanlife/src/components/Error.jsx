f03f31c6c1ada6bb0db126b793123a3dc10274d8
Chris Gibbons <christ487@icloud.com>
2023-07-10 21:50:20 +0100

    add error message into html

--- vanlife/src/components/Error.jsx
+++ vanlife/src/components/Error.jsx
@@ -5,7 +5,10 @@ import { useRouteError } from 'react-router-dom'
 function Error() {
     const error = useRouteError()
   return (
-    <h1>An error occured!</h1>
+    <>
+        <h1>Error: {error.message}</h1>
+        <pre>{error.status} - {error.statusText}</pre>
+    </>
   )
 }
 
